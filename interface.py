7ddc3bf6f76c2bbf04dd2abd995469f05f488cb6
mnobre <mateusm.nobre@gmail.com>
2020-12-25 13:40:03 -0300

    Add files via upload

--- interface.py
+++ interface.py
@@ -1,25 +1,27 @@
-import Data_Collector as dc
 import numpy as np
 import pandas as pd
 import collections
+import Data_Collector as DC
 import Cluster as CT
 import Song_NN_class as NN
 import Playlist_Creator as PC
-ms = dc.map_songs
-mp = dc.map_playlist
-f = dc.get_related
-playlist = 'Swagger'
-# playlist_owner = 'Spotify'
-# playlist_id =
-# não entrar com playlist_ID sem user
-mp(playlist, user=None, playlist_ID=None)
+ms = DC.map_songs
+mp = DC.map_playlist
+f = DC.get_related
+playlist = 'Instrumental Madness'
+playlist_owner = None
+playlist_id = None
+#  não entrar com playlist_ID sem user
+mp(playlist, playlist_owner, playlist_id)
 DF = pd.read_csv(playlist + '.csv')
 a = list(set(DF['artist_id'].values))
+DF = pd.read_csv('tracklist.csv')
+a = list(set(DF['id'].values))
 b = f(a, n=1)
 b = [item for item, count in collections.Counter(
-    b).items() if count > (int(.5 + .25 * len(a)))]
-a.extend(b)
-a = list(set(a))
+    b).items() if count > 1]
+a = list(set(b))
+print(len(a))
 c = pd.DataFrame(a, columns=['id'])
 c.to_csv('tracklist.csv', index=False)
 # h = pd.read_csv('tracklist.csv')
